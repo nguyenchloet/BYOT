1f8a06d7d29e3eadb3f5f39917fe6eed57660231
NGUYEN <35670719+TNg018@users.noreply.github.com>
2022-10-07 15:53:28 -0400

    first commit

---
+++ BYOT-controller-data-cleaning.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+df = pd.read_csv("BYOT.csv")
+
+# get headers from csv file {Date,Workout #,Exercise,Warm Up,Warm Up Sets,Warm Up Reps,Work Sets,Work Reps,Weight,Unit,Muscle Group,Time,Note
+
+print(df)
